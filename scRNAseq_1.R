8ee422a6f1acf7f345d4a7d6d5fa85641743913c
yukato <81909162+ykato0525@users.noreply.github.com>
2023-02-17 18:05:13 +0900

    Update scRNAseq_1.R
    ファイルの前処理のスクリプトを追加しました。

--- scRNAseq_1.R
+++ scRNAseq_1.R
@@ -1,38 +1,52 @@
 library(dplyr)
 library(Seurat)
 library(patchwork)
+library(progress)
 set.seed(1234)
+
+# プログレスバーのための変数
+n <- 100
 
 cat("single cell RNA-seq解析を始めます\n")
 # ディレクトリの設定
+
+path <- "~/analysis/ykato/scRNAseq/data"
+# ディレクトリの表示
+setwd(path)
+cat(list.files(), "\n")
+
 # dataを格納したdirectoryを入力する
-cat("ディレクトリを入力してください")
-input.dir <- readLines(stdin(), n=1)
+input.dir <- readline("ディレクトリを入力してください\n")
 
 # Seuratオブジェクトの構築
 # 必要なら、データを結合するバージョンも作成する
-input.project <- readline("プロジェクト名を入力してください")
-data <- Read10X(data.dir="inpur.dir")
-# ここはdefaultでこの値を使用して良さそう
+input.project <- readline("プロジェクト名を入力してください\n")
+cat("データを読み込みます\n")
+data <- Read10X(data.dir=input.dir)
+
+
+# seurat objectの作成
+cat("Seuratオブジェクトを作成します\n")
 seurat.data <- CreateSeuratObject(counts = data, project = input.project, min.cells = 3, min.features = 200)
 # 確認でseuratオブジェクトを確認する
-length(seurat.data@meta.data$nFeature_RNA)
+cat(length(seurat.data@meta.data$nFeature_RNA))
+cat("\n")
 
 # mtが入るまえのデータであることの確認
-head(seurat.data@meta.data)
-seurat.data[["percent_mt"]] <- PercentageFeatureSet(seurat.data, pattern="^mt-")
+cat("ミトコンドリア由来の遺伝子を除去します\n")
+seurat.data[["percent_mt"]] <- PercentageFeatureSet(seurat.data, pattern="^MT-")
 # mtが入ったことの確認
-cat("ミトコンドリア由来の遺伝子の割合を追加しました")
-head(seurat.data@meta.data)
+cat("ミトコンドリア由来の遺伝子の割合を追加しました\n")
+cat("\n")
 
 # violinplotで
-cat("nFeatureRNA, nCountRNA, percentmtをpngで出力します")
+cat("nFeatureRNA, nCountRNA, percentmtをpngで出力します\n")
 png("VlnPlot_QC_before_filtering.png", width=800, height=500)
-VlnPlot(A549, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+VlnPlot(seurat.data, features = c("nFeature_RNA", "nCount_RNA", "percent_mt"), ncol = 3)
 dev.off()
 
 # 各パラメータの入力を求める
 
 
 # ここまでRdataで保存する
-save(seurat.data, file="seurat_data_01.Rdata")
+#save(seurat.data, file="seurat_data_01.Rdata")
